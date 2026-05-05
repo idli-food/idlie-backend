4026793a32030983dfa377b5e23bad8f42b9648b
a.d.devnandan <a.d.devnandan@gmail.com>
2026-05-05 23:41:45 +0530

    Feat: add user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    phone = models.CharField(max_length=15)
+    username = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    credibility_score = models.FloatField(default=0.5)
+    avatar_url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
