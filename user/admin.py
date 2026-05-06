db1fed4bca86b6b8d24c26e6e0de8fe94005498c
a.d.devnandan <a.d.devnandan@gmail.com>
2026-05-06 16:29:37 +0530

    add admin for user

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,60 @@
-from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'username',
+        'phone',
+        'first_name',
+        'last_name',
+        'diet',
+        'credibility_score',
+        'created_at',
+    )
+
+    search_fields = (
+        'username',
+        'phone',
+        'first_name',
+        'last_name',
+    )
+
+    list_filter = (
+        'diet',
+        'created_at',
+    )
+
+    ordering = ('-created_at',)
+
+    readonly_fields = ('created_at',)
+
+    fieldsets = (
+        ('Basic Information', {
+            'fields': (
+                'username',
+                'phone',
+                'first_name',
+                'last_name',
+                'dob',
+            )
+        }),
+        ('Profile', {
+            'fields': (
+                'bio',
+                'avatar_url',
+                'diet',
+                'food_preference',
+            )
+        }),
+        ('Metrics', {
+            'fields': (
+                'credibility_score',
+                'created_at',
+            )
+        }),
+    )
