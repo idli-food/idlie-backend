65fb8b67e85770365096d3b11b467165dd9bf782
a.d.devnandan <a.d.devnandan@gmail.com>
2026-05-13 01:46:32 +0530

    Feat : added the profile url

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,11 @@ from django.urls import path,include
 
 # Create your views here.
 from .views.login_view import LoginView
+from .views.profle_view import ProfileView
 
 urlpatterns = [
-path("login/",LoginView.as_view(),name="login view")
+
+path("login/",LoginView.as_view(),name="login view"),
+path("profile/",ProfileView.as_view(),name="login view")
+
 ]
