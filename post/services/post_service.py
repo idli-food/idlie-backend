a54597283176750199b83f8f96ee7fdaea0ca369
a.d.devnandan <a.d.devnandan@gmail.com>
2026-05-27 03:36:08 +0530

    refacotored the pre_signed and turned into a public url

--- post/services/post_service.py
+++ post/services/post_service.py
@@ -1,67 +1,42 @@
 from django.db.models import F
-from ..models import Post,Like
-import boto3
+from ..models import Post, Like
 from django.conf import settings
 
 
-
-def get_pre_signed_url(s3_key,expiry_seconds=3600):
-    s3_client = boto3.client(
-        "s3",
-        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-        region_name=settings.AWS_S3_REGION_NAME,
-    )
-
-    url = s3_client.generate_presigned_url(
-        ClientMethod="get_object",
-        Params={
-            "Bucket": settings.AWS_STORAGE_BUCKET_NAME,
-            "Key": s3_key,
-        },
-        ExpiresIn=expiry_seconds,
-    )
-
-    return url
+def get_s3_public_url(s3_key):
+    bucket = settings.AWS_STORAGE_BUCKET_NAME
+    region = settings.AWS_S3_REGION_NAME
+    return f"https://{bucket}.s3.{region}.amazonaws.com/{s3_key}"
 
 
 def check_post_availablity(post_id):
-    return not Post.objects.filter(id=post_id,status=Post.Status.PUBLISHED).exists()
-    
+    return not Post.objects.filter(id=post_id, status=Post.Status.PUBLISHED).exists()
+
 
 def update_post_like_count(post_id):
     total_like_count = Like.objects.filter(post_id=post_id).count()
     Post.objects.filter(id=post_id).update(like_count=total_like_count)
 
 
-
 def set_media_url(post):
-    print(post.raw_s3_key)
-
     try:
-
         if not post.raw_s3_key:
             return None
 
-        url = get_pre_signed_url(post.raw_s3_key)
-        print(url)
-        p = Post.objects.filter(id=post.id).update(media_url = url)
-        print("-------------------")
+        url = get_s3_public_url(post.raw_s3_key)
+        Post.objects.filter(id=post.id).update(media_url=url)
 
     except Post.DoesNotExist:
         return None
 
+
 def set_thumbnail_url(post):
-
     try:
-
         if not post.raw_s3_key:
             return None
 
         url = "https://picsum.photos/200/300"
-
-        Post.objects.filter(id=post.id).update(thumbnail_url = url)
-        print("++++++++++++++")
+        Post.objects.filter(id=post.id).update(thumbnail_url=url)
 
     except Post.DoesNotExist:
         return None
