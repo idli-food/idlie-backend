8e715012352a6a1fa88237a4b72dff4352773928
a.d.devnandan <a.d.devnandan@gmail.com>
2026-05-28 11:41:30 +0530

    explore view url set

--- feed/urls.py
+++ feed/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views.feed_view import FeedView
+from .views.explore_view import ExplorePageView
 
 urlpatterns = [
     path("", FeedView.as_view(),name="feed view"),
+    path("", ExplorePageView.as_view(),name="explore view"),
+
 ]
