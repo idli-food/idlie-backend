aad23429e547fc56565ddb3dbd487c8e49beee9f
a.d.devnandan <a.d.devnandan@gmail.com>
2026-08-17 00:26:14 +0530

    create hotel model

--- hotel/models.py
+++ hotel/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.gis.db import models as gis_models
 
 # Create your models here.
+
+
+
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    location = gis_models.PointField(geography=True, null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
