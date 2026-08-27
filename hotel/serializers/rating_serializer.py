2a1d1d481c6c951616e032c169c64717ff255c5e
a.d.devnandan <a.d.devnandan@gmail.com>
2026-08-27 23:59:11 +0530

    add user data to rating resposne

--- hotel/serializers/rating_serializer.py
+++ hotel/serializers/rating_serializer.py
@@ -2,7 +2,20 @@ from rest_framework import serializers
 from hotel.models import HotelRating, HotelReview
 
 
+class RatingUserSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    username = serializers.CharField()
+    avatar = serializers.SerializerMethodField()
+
+    def get_avatar(self, obj):
+        if hasattr(obj, "profile"):
+            return obj.profile.avatar
+        return None
+
+
 class HotelRatingSerializer(serializers.ModelSerializer):
+    user = RatingUserSerializer(read_only=True)
+
     class Meta:
         model = HotelRating
         fields = [
@@ -16,6 +29,8 @@ class HotelRatingSerializer(serializers.ModelSerializer):
 
 
 class HotelReviewSerializer(serializers.ModelSerializer):
+    user = RatingUserSerializer(read_only=True)
+
     class Meta:
         model = HotelReview
         fields = [
